01273e2ef5c80c4f4870e5fd0070a02eece5871f
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-04-23 08:18:24 -0400

    Updated from Brython Server: 4/23/2018 8:18:12 AM

--- fwordDemo.py
+++ fwordDemo.py
@@ -1,1 +1,8 @@
+#Sam Krimmel
+#4/23/18
+#fwordDemo.py - practice with lists - print out only words that have an f
 
+words = input('Type in some words: ').split(' ') #split string into list when space is seen
+
+for item in words:
+    print(item)
