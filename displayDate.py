ff48137d29980ceeec78ccd5473fc73b0f6bf1f5
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-04-25 08:39:13 -0400

    Updated from Brython Server: 4/25/2018 8:39:13 AM

--- displayDate.py
+++ displayDate.py
@@ -1,1 +1,10 @@
+#Sam Krimmel
+#4/25/18
+#displayDate.py - displays current date
 
+import datetime
+
+day = ['Mondey','Tueday','Wendsay','Thersday','Fridy']
+month = ['January','February','March','April','May','June','July','August','September','October','November','December']
+
+print("To
