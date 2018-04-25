c3641b68058cdd36dab830fd827f5d7c489badb7
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-04-25 08:16:53 -0400

    Updated from Brython Server: 4/25/2018 8:16:53 AM

--- warmup15.py
+++ warmup15.py
@@ -1,1 +1,15 @@
+#Sam Krimmel
+#4/25/18
+#warmup15.py - Make a list of 20 random numbers between 1 and 100, print sum of list, min of list and max of list
 
+from random import randint
+
+numbers = []
+
+for i in range(1,20):
+    numbers.append(randint(1,100))
+
+print(sum(numbers))
+print(min(numbers))
+print(max(numbers))
+    
