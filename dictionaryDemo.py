48fcceb3f08c07c52b400ed2ee91a5824acb3b63
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-04-25 08:10:09 -0400

    Updated from Brython Server: 4/25/2018 8:10:09 AM

--- dictionaryDemo.py
+++ dictionaryDemo.py
@@ -1,1 +1,13 @@
+#Sam Krimmel
+#4/25/18
+#dictionaryDemo.py - more lists practice
 
+words = ['computer','mortify','dog','firetruck','yes','python','cat']
+
+words.sort()
+
+num = int(input("What number word do you want??"))
+if num<=0 or num>= len(words)+1:
+    print("No no no, tsk tsk tsk.")
+else:
+    print(words[num-1])
