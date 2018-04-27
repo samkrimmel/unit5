75a46988f822c0feff810cfce2acfa4893e7a01a
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-04-27 08:27:20 -0400

    Updated from Brython Server: 4/27/2018 8:27:18 AM

--- matrixDemo.py
+++ matrixDemo.py
@@ -2,4 +2,10 @@
 #4/27/18
 #matrixDemo.py - using lists, but better, listception
 
+def printBoard(board):
+    for r in range(0,3):
+        print(board[r])
+
 board = [['a','b','c'],['d','e','f'],['g','h','i']]
+
+printBoard(board)
