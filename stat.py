37e56951d0dad11653129f3b92a010c5f90b4389
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-04-27 08:41:13 -0400

    Updated from Brython Server: 4/27/2018 8:41:11 AM

--- stat.py
+++ stat.py
@@ -7,6 +7,10 @@ while True:
     num = input('Enter a list of numbers, enter q when finished: ')
     if num == 'q':
         break
+        print('Min: ',min(L))
+        print('Max: ',max(L))
+        print('Mean: ',sum(L)/len(L))
     else:
         L.append(num)
 
+print(L)
