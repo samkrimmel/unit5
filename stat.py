192a377bdbf7b1f6e8fb7a5d015d18ab13e2de19
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-04-27 08:37:20 -0400

    Updated from Brython Server: 4/27/2018 8:37:18 AM

--- stat.py
+++ stat.py
@@ -2,4 +2,11 @@
 #4/25/18
 #stat.py
 
+L = []
+while True:
+    num = input('Enter a list of numbers, enter q when finished: ')
+    if num == 'q':
+        break
+    else:
+        L.append(num)
 
