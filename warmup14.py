83f9b94696bccc28ac53011b326191b6a705b9db
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-04-30 08:27:55 -0400

    Updated from Brython Server: 4/30/2018 8:27:51 AM

--- warmup14.py
+++ warmup14.py
@@ -1,1 +1,9 @@
+#Sam Krimmel
+#4/30/18
+#warmup14.py - asks user for their first and last name and tells them their initals
 
+name = input('Vhat iss yoour naaame??').split(' ')
+quest = input('Vhat... is your QuEsT??')
+color = input('WHat.. is your favourite colour??')
+
+print('Your initals are', name[0][0], name[1][0]
