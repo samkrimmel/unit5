637038111bcb43a1d35da053523ed1c163ce9a10
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-04-30 08:14:51 -0400

    Updated from Brython Server: 4/30/2018 8:14:47 AM

--- antsDemo.py
+++ antsDemo.py
@@ -10,6 +10,10 @@ ANTS = 10
 WIDTH = 900
 HEIGHT = 600
 
+def step():
+    for ant in data['antList']:
+        ant.x += 1
+        ant.y += 1
 
 if __name__ == '__main__':
     
@@ -22,4 +26,4 @@ if __name__ == '__main__':
     for i in range(ANTS):
         data['antList'].append(Sprite(ant,(randint(1,WIDTH),randint(1,HEIGHT))))
     
-    App().run()
+    App().run(step)
