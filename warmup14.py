44668a9cbef06422941e19d0de0476f5ce6a81f4
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-04-30 08:29:59 -0400

    Updated from Brython Server: 4/30/2018 8:29:55 AM

--- warmup14.py
+++ warmup14.py
@@ -2,6 +2,6 @@
 #4/30/18
 #warmup14.py - asks user for their first and last name and tells them their initals
 
-name = input('What is your name??').split(' ')
+name = input('What is your name?').split(' ')
 
 print('Your initals are', name[0][0], name[1][0])
