d1d27575ff4f2e88f3643aa9f7798455d643db17
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-05-02 08:31:02 -0400

    Updated from Brython Server: 5/2/2018 8:30:57 AM

--- cocktailSortDemo.py
+++ cocktailSortDemo.py
@@ -1,1 +1,32 @@
+#Sam Krimmel
+#5/2/18
+#cocktailSortDemo.py - implementing cocktail sort
 
+from random import randint
+from time import time
+
+N = 10 #how many numbers will be sorted
+
+def mySort(A):
+    return A
+    
+if __name__ == '__main__':
+
+    #make a list of N random numbers between 1 and N
+    numbers = [0]*N
+    for i in range(N):
+        numbers[i] = randint(1,N)
+    
+    pythonSort = sorted(numbers) #Python's sort
+    
+    #time how long your sort takes
+    t1 = time()
+    numbers = mySort(numbers)
+    t2 = time()
+    
+    #print whether the sort worked or not
+    try:
+        assert(numbers == pythonSort)
+        print('Your sort took', t2-t1, 'seconds')
+    except:
+        print('Your sort did not work')
