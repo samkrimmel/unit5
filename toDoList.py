cb478694ca9d1aa86ede4456a09315bb6deea13a
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-05-04 08:43:01 -0400

    Updated from Brython Server: 5/4/2018 8:42:54 AM

--- toDoList.py
+++ toDoList.py
@@ -2,4 +2,12 @@
 #5/4/18
 #toDoList.py - make a list of things to of the do!
 
+list = []
 
+while True:
+    print("Valid Commands: add, delete, print, quit")
+    inp = input()
+    
+    if inp == "quit":
+        break
+    
